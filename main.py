ef2d27fb1ee626a9eb37583a28b4e8e9fceb3342
Mizzigno <dysach23@gmail.com>
2026-09-21 18:20:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 meme_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso",
             "LOL": "Una respuesta común a algo gracioso",
-            "67": "no tiene un significado fijo y suele usarse para decir «más o menos» o como broma"
+            "67": "no tiene un significado fijo y suele usarse para decir «más o menos» o como broma",
+            "ROFL": "una respuesta a una broma"
+            "SHEESH" - "ligera desaprobación"
+            "CREEPY" - "aterrador, siniestro"
+            "AGGRO" - "ponerse agresivo/enojado"
             }
 word = input("Ingresa la palabra que deseas conocer")
 
